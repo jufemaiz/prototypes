1a23508779bca42f2891fb602793e90955974b85
Aaron <me@aaronlinkous.com>
2013-06-21 14:19:37 -0700

    misc. fart button

---
+++ scss/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE  HTML>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/css.css" media="screen" />
+
+	</head>
+	<body>
+
+		<a href="#" class="primary button_link">Button Link</a>
+
+		<button class="secondary">Real Button</button>
+
+		<input class="alternate" type="button" value="Input Button" />
+
+		<div class="clear"></div>
+
+		<a href="#" class="primary button_link dbl">Button Link</a>
+
+		<button class="secondary dbl">Real Button</button>
+
+		<input class="alternate dbl" type="button" value="Input Button" />
+
+
+		<div class="clear"></div>
+
+		<a href="#" class="primary full_width button_link dbl">Button Link</a>
+
+		<button class="secondary full_width dbl">Real Button</button>
+
+		<input class="alternate full_width dbl" type="button" value="Input Button" />
+
+		<div class="clear"></div>
+
+	</body>
+</html>
