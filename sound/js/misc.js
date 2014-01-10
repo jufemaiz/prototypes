f9711cbda51d327614469cd952fa92b25666cc45
Aaron Linkous <aaron@bellmobi.com>
2014-01-09 17:30:02 -0800

    finally got FL first reflection correct

--- sound/js/misc.js
+++ sound/js/misc.js
@@ -1,4 +1,4 @@
-var dims = [], room_w, room_h, room_l;
+var dims = [], speaker_pos = [], scale = 3, room_w, room_h, room_l, system;
 
 function intersection(array1, array2) {
 	return array1.filter(function (n) { return array2.indexOf(n) !== -1; });
@@ -54,18 +54,55 @@ function room_modes() {
 		modes.push(new_modes);
 	});
 
-/* 	Now that we have the modes, we need to show the emphasized nodes */
+/* 	Now that we have the modes, we need to find the emphasized nodes */
 	multi_intersection(modes)
+}
+
+function speaker_placement(){
+	$(".reflection").remove();
+
+	var listener_x = $("#listener #x").val();
+	var listener_z = $("#listener #z").val();
+
+	var fl_x = $("#fl #x").val();
+	var fl_z = $("#fl #z").val();
+
+	$("#listener").css({left: listener_x*scale, top: listener_z*scale});
+	$("#fl").css({left: fl_x*scale, top: fl_z*scale});
+
+	fl = (((parseInt(listener_z)-parseInt(fl_z)) * parseInt(listener_x))/(parseInt(fl_x)+parseInt(listener_x)));
+
+	$('<div id="fl_r" class="reflection"></div>').appendTo("#room").css({top: ((parseInt(listener_z)-parseInt(fl)))*scale});
+
+	console.log(fl);
+}
+
+function build_room() {
+	$("#room").show().css({width: dims[0]*scale, height:dims[2]*scale});
+
+	speaker_placement();
 }
 
 $(document).ready(function(){
 	$("#room_dimensions").on("click", function(e) {
-		room_w = $("#room_width").val() || 0;
-		room_h = $("#room_height").val() || 0;
-		room_l = $("#room_length").val() || 0;
-
+		room_w = $("#room_width").val();
+		room_h = $("#room_height").val();
+		room_l = $("#room_length").val();
 		dims = [room_w,room_h,room_l];
 
+		system = $("#system").val() || 7;
 		room_modes();
+		build_room();
+	});
+
+	$("#room .speaker, #room #listener").on("click", function(){
+		$("#room .speaker, #room #listener").not(this).children(".inputs").hide();
+		$(this).children(".inputs").show();
+	});
+
+	$("#room input").on("change", function(e) {
+		speaker_pos = [];
+
+		speaker_placement();
 	});
 });
